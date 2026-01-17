b825edda5fa3a108eaddb960939dc6b077fa1f51
Krzysztof Kania <kaniakriskox@outlook.com>
2026-01-17 21:35:05 +0100

    dodanie podstawowej struktury

---
+++ app.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div className="container">
+            <h1>SkyCast Weather App</h1>
+            <p>React działa poprawnie! okok</p>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
